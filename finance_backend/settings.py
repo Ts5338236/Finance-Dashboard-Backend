ee2102c1f21aec3190cb18ba0d3db52634ca3b64
Tushar Sharma <ts5338236@gmail.com>
2026-04-03 13:20:56 +0530

    Move DB to /tmp for Vercel support

--- finance_backend/settings.py
+++ finance_backend/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'finance_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3' if DEBUG else '/tmp/db.sqlite3',
     }
 }
 
